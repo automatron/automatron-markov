e43572dc64b152a2c5c2cf44fcfea820e3843740
Ingmar Steen <iksteen@gmail.com>
2014-03-02 13:15:18 +0100

    Depend on client.msg extracting the nickname

--- twisted/plugins/automatron_markov_plugin.py
+++ twisted/plugins/automatron_markov_plugin.py
@@ -1,5 +1,5 @@
 from twisted.internet import defer
-from automatron_markov import build_prefix
+from automatron_markov import build_prefix, DEFAULT_CHAIN_LENGTH
 from automatron_markov.learn import learn_phrase, parse_line
 from automatron_markov.reply import build_reply
 from automatron_redis.txredisapi import lazyConnection
@@ -24,36 +24,54 @@ class AutomatronMarkovPlugin(object):
         self.redis = lazyConnection(**redis_config)
 
     def on_command(self, client, user, command, args):
-        nickname = client.parse_user(user)[0]
+        if command != 'markov':
+            return
 
-        if command == 'markov-learn':
-            if len(args) != 2 or args[1] not in ('true', 'false'):
-                client.msg(nickname, 'Syntax: markov-learn <channel> <true/false>')
-            else:
-                self._update_config(client, user, args[0], 'learn', args[1])
-            return STOP
-        elif command == 'markov-reply':
-            if len(args) != 2 or args[1] not in ('true', 'false'):
-                client.msg(nickname, 'Syntax: markov-reply <channel> <true/false>')
-            else:
-                self._update_config(client, user, args[0], 'reply', args[1])
-            return STOP
-        elif command == 'markov-namespace':
-            if len(args) != 2:
-                client.msg(nickname, 'Syntax: markov-namespace <channel> <namespace>')
-            else:
-                self._update_config(client, user, args[0], 'namespace', args[1])
-            return STOP
+        if not args:
+            self._help(client, user)
+        else:
+            subcommand, args = args[0], args[1:]
+            self._on_subcommand(client, user, subcommand, args)
+
+        return STOP
+
+    def _help(self, client, user):
+        for line in '''Syntax: markov <task> <args...>
+Available tasks:
+markov learn <true/false> <channel...>
+markov reply <true/false> <channel...>
+markov namespace <true/false> <channel...>'''.split('\n'):
+            client.msg(user, line)
 
     @defer.inlineCallbacks
-    def _update_config(self, client, user, channel, key, value):
-        nickname = client.parse_user(user)[0]
-        if not (yield self.controller.config.has_permission(client.server, channel, user, 'markov')):
-            client.msg(nickname, 'You\'re not authorized to do that.')
-            defer.returnValue(STOP)
+    def _on_subcommand(self, client, user, subcommand, args):
+        if subcommand in ('learn', 'reply') and len(args) >= 2 and args[0] in ('true', 'false'):
+            if (yield self._verify_permissions(client, user, args[1:])):
+                self._on_update_setting(client, user, args[1:], subcommand, args[0])
+        elif subcommand == 'namespace' and len(args) >= 2:
+            if (yield self._verify_permissions(client, user, args[1:])):
+                self._on_update_setting(client, user, args[1:], subcommand, args[0])
 
-        self.controller.config.update_plugin_value(self, client.server, channel, key, value)
-        client.msg(nickname, 'OK')
+    @defer.inlineCallbacks
+    def _verify_permissions(self, client, user, channels):
+        for channel in channels:
+            if not (yield self.controller.config.has_permission(client.server, channel, user, 'youtube-playlist')):
+                client.msg(user, 'You\'re not authorized to change settings for %s' % channel)
+                defer.returnValue(False)
+
+        defer.returnValue(True)
+
+    def _on_update_setting(self, client, user, channels, key, value):
+        for channel in channels:
+            self.controller.config.update_plugin_value(
+                self,
+                client.server,
+                channel,
+                key,
+                value
+            )
+
+        client.msg(user, 'OK')
 
     def on_message(self, client, user, channel, message):
         return self._on_message(client, user, channel, message)
@@ -63,10 +81,6 @@ class AutomatronMarkovPlugin(object):
         config = yield self.controller.config.get_plugin_section(self, client.server, channel)
         prefix = build_prefix(config.get('namespace'))
 
-        if config.get('learn', 'false') == 'true':
-            self._learn(prefix, message)
-            return
-
         if config.get('reply', 'false') == 'true' and \
                 channel != client.nickname and \
                 message.startswith(client.nickname + ':'):
@@ -74,6 +88,10 @@ class AutomatronMarkovPlugin(object):
             d = build_reply(self.redis, prefix, message.split(':', 1)[1].strip())
             d.addCallback(lambda reply: client.msg(channel, '%s: %s' % (nickname, reply or 'I got nothing...')))
             defer.returnValue(STOP)
+
+        if config.get('learn', 'false') == 'true':
+            self._learn(prefix, message)
+            return
 
     @defer.inlineCallbacks
     def _learn(self, prefix, message):
